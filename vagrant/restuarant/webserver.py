26864f80dec85dccb26a8f29d090038a036f32e3
willa75 <anthonywi99@yahoo.com>
2015-11-26 08:13:52 -0500

    Actally removed puppy shleter dir and modified webserver

--- vagrant/restuarant/webserver.py
+++ vagrant/restuarant/webserver.py
@@ -1,6 +1,8 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 class webserverHandler(BaseHTTPRequestHandler):
+	
 	def do_GET(self):
 		try:
 			if self.path.endswith("/hello"):
@@ -9,13 +11,66 @@ class webserverHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 
 				output = ""
-				output += "<html><body>Hello!</body></html>"
+				output += "<html><body>"
+				output += "Hello!"
+				output += """<form method='POST' enctype='multipart/form-data'
+					action='/hello'><h2>What would you like to say?</h2><input 
+					name='message' type='text'><input type='submit' value='submit'>
+					</form>"""
+				output += "</body></html>"
+				self.wfile.write(output)
+				print output
+				return
+			if self.path.endswith("/hola"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>"
+				output += """&#161Hola!<a href='/hello'>Back to 
+				Hello</a>"""
+				output += """<form method='POST' enctype='multipart/form-data'
+					action='/hello'><h2>What would you like to say?</h2><input 
+					name='message' type='text'><input type='submit' value='submit'>
+					</form>"""
+				output += "</body></html>"
 				self.wfile.write(output)
 				print output
 				return
 
 		except IOError:
 			self.sned_error(404, "File Not Found %s" % self.path)
+
+	def do_POST(self):
+		try:
+			self.send_response(301)
+			self.send_header('Content-type', 'text/html')
+			self.send_headers()
+
+			ctype, pdict = cgi.parse_header(
+				self.headers.getheader('content-type'))
+			if ctype == 'multipart/form-data':
+				fields=cgi.parse_multipart(self.rfile, pdict)
+				messagecontent = fields.get('message')
+
+			output = ""
+			output += "<html><body>"
+			output += "<h2> Okay, how about this: </h2>"
+			output += "<h1> %s </h1>" % messagecontent[0]
+
+			output += """<form method='POST' enctype='multipart/form-data'
+				action='/hello'><h2>What would you like to say?</h2><input 
+				name='message' type='text'><input type='submit' value='submit'>
+				</form>"""
+			output += "</body></html>"
+			self.rfile.write(output)
+			print output
+
+		except:
+			pass
+
+
 
 def main():
 	try:
